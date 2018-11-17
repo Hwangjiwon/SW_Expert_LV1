18ff62f45e595c65664fc835aa56340002cd2701
Jiwon <googims33@naver.com>
2018-11-17 09:15:38 +0900

    no_2047

---
+++ no_2047/MainClass.java
@@ -0,0 +1,17 @@
+package no_2047;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		str = str.toUpperCase();
+		System.out.println(str);
+		sc.close();
+	}
+
+}
